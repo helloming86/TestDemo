26a84f5bc81198a6b14508a970dc079a5bb885c2
helloming86 <helloming86@qq.com>
2016-09-20 16:39:49 +0800

    d

--- features/step_definitions/login.rb
+++ features/step_definitions/login.rb
@@ -3,7 +3,7 @@ require 'selenium-webdriver'
 require 'rspec/expectations'
 
 #启动浏览器打开登录页面
-$caps = Selenium::WebDriver::Remote::Capabilities.chrome
+$caps = Selenium::WebDriver::Remote::Capabilities.firefox marionette: true, firefox_binary: "/path/to/firefox"
 $huburl = 'http://localhost:4444/wd/hub'
 
 假如(/^打开登录页面$/) do
